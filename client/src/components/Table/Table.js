74301917a6dd46452cf2f12f5f2607e585f9a485
jwestcott9 <jwestcott9@gmail.com>
2019-08-23 20:14:09 -0600

    some fixed modal action

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./style.scss";
 import TableData from "./tableData"
-
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 function Table (props){
     console.log(props)
     return(
        
             <tr>
                 <TableData>{props.name}</TableData>
-                <TableData>{props.summary}</TableData>
+                {props.summary?
+                <TableData>{ReactHtmlParser(props.summary)}</TableData>
+                :
+                <TableData>{props.amount}</TableData>}
                 <TableData>{props.image}</TableData>
             </tr>
     
